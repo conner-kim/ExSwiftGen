c21c664495a307b7adb73eb859990422835d1a61
Conner Kim <dudrjs145@naver.com>
2023-01-03 00:50:31 +0900

    SwiftGen으로 리소스 관리 추가

--- ExSwiftGen/ViewController.swift
+++ ExSwiftGen/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = AssetRes.Colors.classicBlue.color
+        self.imageView.image = AssetRes.Assets.icoBox.image
+        self.label.text = StringRes.Bananas.owner(123, "오호호")
     }
-
-
 }
-
